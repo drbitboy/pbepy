217ce093c0457a545f85f47e4c9ed55558a34d26
Brian Carcich <briantcarcich@gmail.com>
2023-11-09 18:18:43 -0500

    more edits

--- findtca.py
+++ findtca.py
@@ -1,11 +1,14 @@
+import sys
 import numpy
 import datetime
 import collections
 import spiceypy as sp
 
+werr = sys.stderr.write
 try: clight
 except:
   clight = sp.clight()
+  spd = sp.spd()
 
 
 #######################################################################n
@@ -33,15 +36,17 @@ ftca_getrange:  FINDTCA support function, get range from observer (obs)
                 observer to the target, and light time
 
 """
-  ltime = -1d0 / cspice_clight()
+  ltime = -1e0 / sp.clight()
   try:
-    cspice_spkezr,targ,et,'J2000',ltcorr,obs,stOut,ltime
-    return ltime*cspice_clight(),stOut,ltime
+    sp.spkezr,targ,et,'J2000',ltcorr,obs,stOut,ltime
+    return ltime*sp.clight(),stOut,ltime
   except:
     import traceback
     traceback.print_exc()
   return -1e0,sp.vequ([0]*6,6),-1e0/clight
 
+
+########################################################################
 def ftca_getopt(defawlt, arg, poolvars):
   """
 FTCA_GETOPT:  FINDTCA support function, select option
@@ -54,7 +59,7 @@ FTCA_GETOPT:  FINDTCA support function, select option
 
 """
 
-  if not (arg is None): return,Arg,'ARGUMENT'
+  if not (arg is None): return Arg,'ARGUMENT'
 
   if not isinstance(poolvars,collections.Sequence):
     return ftcs_getopt(defawlt,arg,[poolvars])
@@ -68,32 +73,32 @@ FTCA_GETOPT:  FINDTCA support function, select option
       elif typ is float: Vals = sp.gdpool(poolvar,0,999)
       else             : Vals = sp.gipool(poolvar,0,999)
 
-      if len(Vals) eq 1L then Vals = Vals.pop()
-      return, Vals,'KERNELPOOL'
+      if len(Vals) == 1: Vals = Vals.pop()
+      return Vals,'KERNELPOOL'
     except:
       continue
 
-  return,defawlt,'DEFAULT'
+  return defawlt,'DEFAULT'
 
-"""
 
 ########################################################################
 ########################################################################
 ### FINDTCA:  Main function
 ########################################################################
 ########################################################################
-function findtca, targArg, obsArg, utcEst=utcEstArg
-                , VinfTarg=VinfTarg
-                , VinfEtOffset=VinfEtOffset
-                , VinfEtStep=VinfEtStep
-                , VinfEtNum=VinfEtNum
-                , ltcorrIn=ltcorrArg
-                , kernels=kernels
-                , nounloadkernels=nounloadkernels
-                , debug=debug
-  dodebug = keyword_set(debug)
+def findtca(targArg=None
+           ,obsArg=None
+           ,utcEst=utcEstArg
+           ,VinfTarg=VinfTarg
+           ,VinfEtOffset=VinfEtOffset
+           ,VinfEtStep=VinfEtStep
+           ,VinfEtNum=VinfEtNum
+           ,ltcorrIn=ltcorrArg
+           ,kernels=kernels
+           ,nounloadkernels=nounloadkernels
+           ):
 
-  ftca_furnsh,kernels,debug=debug
+  ftca_furnsh(kernels)
 
   targ = ftca_getopt('PLUTO', targArg, 'FINDTCA_TARGET')
   obs = ftca_getopt('-98', obsArg, 'FINDTCA_OBSERVER',which=wObs)
@@ -105,209 +110,200 @@ function findtca, targArg, obsArg, utcEst=utcEstArg
   defawlt = strcompress( 'JD ' + string(julday(),f='(f30.1)') ) + ' TDB'
   utcEst = ftca_getopt(defawlt, utcEstArg, 'FINDTCA_UTCEST',which=wUtcEst)
 
-  if wUtcEst eq 'DEFAULT' then begin
-    delEst=7d0
-  endif else begin
-    delEst=1d0
-  endelse
+  ### Convert delEst to s
 
-  ### Find out if LEAPSECONDS have been loaded
+  delEst = spd * (wUtcEst == 'DEFAULT' and 7e0 or 1e0)
 
-  lskEr=0L
-  catch,lskEr
-  if lskEr eq 0L then begin
-    ###cspice_deltet,0d0,'ET',d
-    ###catch,/cancel
-    cspice_str2et,utcEst,etEst
-    catch,/cancel
-  endif else begin
-    catch,/cancel
-    cspice_tparse,utcEst,etEst ,tpErrmsg
-    message,/continue,'WARNING:  NO LEAPSECOND KERNEL AVAILABLE'
-    if strtrim(tpErrmsg,2) ne '' then message,/continue,'TPARSE:  ' + tpErrmsg
-  endelse
+  try:
 
-  ### Convert UTC estimate to ET (s past J2000), delEst to s
+    ### Find out if LEAPSECONDS have been loaded
+    ### Convert UTC estimate to ET (s past J2000)
 
-  delEst = delEst * cspice_spd()
+    try:
+      etEst = sp.str2et(utcEst)
+    except:
+      etEst,tpErrmsg = sp.tparse(utcEst)
+      werr('WARNING:  NO LEAPSECOND KERNEL AVAILABLE\n')
+      if tpErrmsg.strip() != '': werr(f'TPARSE:  {tpErrmsg}\n')
 
-  ### Go 250 steps either side of estimate, take first local minimum range
+    ### Go 250 steps either side of estimate, take first local minimum range
 
-  iCtr=1000L
-  nRanges = iCtr * 2L + 1L
-  ranges = make_array(nRanges,val=-1d0)
+    iCtr=1000
+    nRanges = iCtr * 2 + 1
+    ranges = numpy.zeros(nRanges) - 1e0
 
-  iMin = -1L
-  i = 0L
-  while i le iCtr and iMin lt 0L do begin
-    i0 = iCtr+i
-    ranges[i0] = ftca_getrange(targ,obs,etEst+(delEst*i),ltcorrIn=ltcorrArg)
-    if min(ranges[i0-2L:i0]) gt -1d0 then begin
-      if min(ranges[[i0-2L,i0]]) ge ranges[i0-1L] then iMin = i0-1L
+    iMin = -1
+    i = 0
+    while i <= iCtr and iMin < 0:
+      i0 = iCtr+i
+      ranges[i0] = ftca_getrange(targ,obs,etEst+(delEst*i),ltcorrIn=ltcorrArg)
+      if min(ranges[i0-2:i0]) > -1e0:
+        if min(ranges[[i0-2,i0]]) >= ranges[i0-1]: iMin = i0-1
+
+      if iMin < 0:
+        i0 = iCtr-i
+        ranges[iCtr-i] = ftca_getrange(targ,obs,etEst-(delEst*i)
+                                      ,ltcorrIn=ltcorrArg)
+        if min(ranges[i0:i0+2]) > -1e0:
+          if min(ranges[[i0+2,i0]]) >= ranges[i0+1]: iMin = i0+1
+
+      i += 1
+
+    ### If NO success at any time and ltcorrArg was not specified
+    ### (so FTCA_GETRANGE defaulted to 'LT' correction), then try
+    ### repeating the calculations without the correction
+
+    if iMin < 0:
+
+      ### Fail with an error message if ltcorrArg was specified ...
+
+      if not (ltcorrArg is None:
+        self.msg = 'Could not find TCA'
+        return
+
+      ### else call self recursively with no light time correction ('NONE')
+
+      self.msg = 'WARNING:  FAILED WITH LIGHT TIME CORRECTION# RE-TRYING WITHOUT ...'
+
+      rtn = findtca( targ, obs, utcEst=utcEstArg, ltcorrIn='NONE', debug=debug)
+      if nounloadkernels: ftca_furnsh,kernels,/unload
+      return
+
     endif
-    if iMin lt 0L then begin
-      i0 = iCtr-i
-      ranges[iCtr-i] = ftca_getrange(targ,obs,etEst-(delEst*i)
-                                    ,ltcorrIn=ltcorrArg)
-      if min(ranges[i0:i0+2L]) gt -1d0 then begin
-        if min(ranges[[i0+2L,i0]]) ge ranges[i0+1L] then iMin = i0+1L
+
+    ### Use Newton-Raphson to find TCA
+
+    etEst = etEst + (iMin-iCtr) * delEst
+    range = ranges[iMin]
+    md = 0e0
+    delEstActual = delEst
+
+    i=0
+    while i <= 10 and delEst ne 0e0 and delEstActual ne 0e0 do begin
+      oldRange = range
+      oldEt = etEst
+      range = ftca_getrange(targ,obs,oldEt,stOut=stOut,ltcorrIn=ltcorrArg)
+      p=stOUt[0:2]
+      v=stOut[3:5]
+      delEst = - sp.vdot(p,v) / total(v*v)
+      etEst = oldEt + delEst
+      delEstActual = etEst - oldEt
+      if dodebug then begin
+        oldMd = md
+        sp.vperp, p, v, mdVec
+        md = sp.vnorm(mdVec)
+        if i == 0 then begin
+          print, 'Iter', 'deltaEtCalc', 'deltaEtActual'
+               , 'deltaMissDist', 'MissDist'
+               , f='(a4,4a16)'
+        endif else begin
+          print,i,delEst,etEst-oldEt,md-oldMd, md
+               , f='(i4,4g16.8)'
+        endelse
       endif
-    endif
+      i=i+1
+    endwhile
 
-    i = i + 1L
-  endwhile
+    if delEstActual ne 0e0 then oldRange = range
+    range = ftca_getrange(targ,obs,etEst,stOut=stOut,ltcorrIn=ltcorrArg
+                         ,ltcorrOut=ltcorrOut
+                         ,ltimeOut=ltimeOut)
 
-  ### If NO success at any time and ltcorrArg was not specified
-  ### (so FTCA_GETRANGE defaulted to 'LT' correction), then try
-  ### repeating the calculations without the correction
+    targPosJ2k = stOut[0:2]
+    targVelJ2k = stOut[3:5]
+    obsPosJ2k = -targPosJ2k
+    obsVelJ2k = -targVelJ2k
 
-  if iMin lt 0L then begin
+    ### Define reference frame as:
+    ###   XY plane contains obs to targ and obs velocity
+    ###   +X = vector to target from observer at CA
+    ###   +Y near observer velocity
 
-    ### Fail with an error message if ltcorrArg was specified ...
+    sp.ucrss, targPosJ2k, obsVelJ2k, trajPlaneZ     ### +X cross ~+Y = +Z
+    sp.ucrss, trajPlaneZ, targPosJ2k, trajPlaneY    ### +Z cross +X  = +Y
+    sp.ucrss, trajPlaneY, trajPlaneZ, trajPlaneX    ### +Y cross +Z  = +X
 
-    if n_elements(ltcorrArg) eq 1L then message,'Could not find TCA'
+    sunRange = ftca_getrange('SUN',targ,etEst-ltimeOut,stOut=stSun
+                         ,ltcorrIn=ltcorrArg
+                         ,ltimeOut=ltimeOut)
 
-    ### else call self recursively with no light time correction ('NONE')
+    if sunRange > -1e0 then begin
+      sunPosJ2k = stSun[0:2]
+      sunStatus = 'OK'
+    endif else begin
+      sunPosJ2k = obsVelJ2k
+      sunStatus = 'SPICE CALL FAILED# USING UPTRACK'
+    endelse
 
-    message,/continue
-           ,'WARNING:  FAILED WITH LIGHT TIME CORRECTION# RE-TRYING WITHOUT ...'
+    sunAzElRFb = cv_coord( /degree, /to_sph
+                         , from_rec=[ sp.vdot(trajPlaneX,sunPosJ2k)
+                                    , sp.vdot(trajPlaneY,sunPosJ2k)
+                                    , sp.vdot(trajPlaneZ,sunPosJ2k)
+                                    ]
+                         )
 
-    rtn = findtca( targ, obs, utcEst=utcEstArg, ltcorrIn='NONE', debug=debug)
+    jdTdbTca=sp.j2000()+etEst/sp.spd()
+    caldat,jdTdbTca,month,day,year,hour,minute,seconds
+    month=strmid('JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC',month*3-3,3)
+    TdbTimeIdl = string(year,' ',month,' ',day,f='(i4.4,a,a,a,i2.2)' )
+                 + ' '
+                 + string( hour, ':',minute,':',seconds, f='(2(i2.2,a),f06.3)')
+                 + ' TDB (IDL CALDAT)'
+
+    sp.etcal,etEst,CalTimeSpice
+
+    utcEr=0
+    catch,utcEr
+    if utcEr == 0 then begin
+      sp.et2utc,etEst,'C',3,UtcTimeBare
+      sp.timout,etEst,'YYYY MON DD HR:MN:SC.### TDB (SPICE TIMOUT)::TDB',999
+                  ,TdbTimeSpice
+      catch,/cancel
+      UtcTime=UtcTimeBare + ' UTC (SPICE ET2UTC)'
+    endif else begin
+      catch,/cancel
+      UtcTime = 'LEAPSECOND-Kernel missing?'
+      TdbTimeSpice = UtcTime
+    endelse
+
+    xxx =
+    {            ET:  etEst
+    , MISS_DISTANCE:  sp.vnorm(stOut[0:2])
+    ,         SPEED:  sp.vnorm(stOut[3:5])
+    ,        TARGET:  targ
+    ,           OBS:  obs
+    ,   OBS_POS_J2k:  obsPosJ2k
+    ,   OBS_VEL_J2k:  obsVelJ2k
+    ,   SUN_POS_J2k:  sunPosJ2k
+    ,   SUN_AZEL_FB:  sunAzElRFb[0:1]
+    ,    SUN_STATUS:  sunStatus
+    ,       LT_CORR:  ltcorrOut
+    ,       TCA_ERR:  abs(delEst)
+    ,   CA_DIST_ERR:  abs(range-oldRange)
+    ,      JDTDBTCA:  jdTdbTca
+    ,    TDBTIMEIDL:  TdbTimeIdl
+    ,  CALTIMESPICE:  CalTimeSpice + ' TDB (SPICE ETCAL)'
+    ,  TDBTIMESPICE:  TdbTimeSpice
+    ,       UTCTIME:  UtcTime
+    }
+
+    tcaOut = create_struct( xxx
+    , 'tofCalc', findvinf( xxx
+                         , VinfTarg
+                         , VinfEtOffset
+                         , VinfEtStep
+                         , VinfEtNum
+                         , debug=debug
+                         )
+    )
+
     if not keyword_set(nounloadkernels) then ftca_furnsh,kernels,/unload
-    return, rtn
 
-  endif
+    return###, tcaOut
 
-  ### Use Newton-Raphson to find TCA
 
-  etEst = etEst + (iMin-iCtr) * delEst
-  range = ranges[iMin]
-  md = 0d0
-  delEstActual = delEst
 
-  i=0L
-  while i le 10L and delEst ne 0d0 and delEstActual ne 0d0 do begin
-    oldRange = range
-    oldEt = etEst
-    range = ftca_getrange(targ,obs,oldEt,stOut=stOut,ltcorrIn=ltcorrArg)
-    p=stOUt[0:2]
-    v=stOut[3:5]
-    delEst = - cspice_vdot(p,v) / total(v*v)
-    etEst = oldEt + delEst
-    delEstActual = etEst - oldEt
-    if dodebug then begin
-      oldMd = md
-      cspice_vperp, p, v, mdVec
-      md = cspice_vnorm(mdVec)
-      if i eq 0L then begin
-        print, 'Iter', 'deltaEtCalc', 'deltaEtActual'
-             , 'deltaMissDist', 'MissDist'
-             , f='(a4,4a16)'
-      endif else begin
-        print,i,delEst,etEst-oldEt,md-oldMd, md
-             , f='(i4,4g16.8)'
-      endelse
-    endif
-    i=i+1
-  endwhile
-
-  if delEstActual ne 0d0 then oldRange = range
-  range = ftca_getrange(targ,obs,etEst,stOut=stOut,ltcorrIn=ltcorrArg
-                       ,ltcorrOut=ltcorrOut
-                       ,ltimeOut=ltimeOut)
-
-  targPosJ2k = stOut[0:2]
-  targVelJ2k = stOut[3:5]
-  obsPosJ2k = -targPosJ2k
-  obsVelJ2k = -targVelJ2k
-
-  ### Define reference frame as:
-  ###   XY plane contains obs to targ and obs velocity
-  ###   +X = vector to target from observer at CA
-  ###   +Y near observer velocity
-
-  cspice_ucrss, targPosJ2k, obsVelJ2k, trajPlaneZ     ### +X cross ~+Y = +Z
-  cspice_ucrss, trajPlaneZ, targPosJ2k, trajPlaneY    ### +Z cross +X  = +Y
-  cspice_ucrss, trajPlaneY, trajPlaneZ, trajPlaneX    ### +Y cross +Z  = +X
-
-  sunRange = ftca_getrange('SUN',targ,etEst-ltimeOut,stOut=stSun
-                       ,ltcorrIn=ltcorrArg
-                       ,ltimeOut=ltimeOut)
-
-  if sunRange gt -1d0 then begin
-    sunPosJ2k = stSun[0:2]
-    sunStatus = 'OK'
-  endif else begin
-    sunPosJ2k = obsVelJ2k
-    sunStatus = 'SPICE CALL FAILED# USING UPTRACK'
-  endelse
-
-  sunAzElRFb = cv_coord( /degree, /to_sph
-                       , from_rec=[ cspice_vdot(trajPlaneX,sunPosJ2k)
-                                  , cspice_vdot(trajPlaneY,sunPosJ2k)
-                                  , cspice_vdot(trajPlaneZ,sunPosJ2k)
-                                  ]
-                       )
-
-  jdTdbTca=cspice_j2000()+etEst/cspice_spd()
-  caldat,jdTdbTca,month,day,year,hour,minute,seconds
-  month=strmid('JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC',month*3-3,3)
-  TdbTimeIdl = string(year,' ',month,' ',day,f='(i4.4,a,a,a,i2.2)' )
-               + ' '
-               + string( hour, ':',minute,':',seconds, f='(2(i2.2,a),f06.3)')
-               + ' TDB (IDL CALDAT)'
-
-  cspice_etcal,etEst,CalTimeSpice
-
-  utcEr=0L
-  catch,utcEr
-  if utcEr eq 0L then begin
-    cspice_et2utc,etEst,'C',3,UtcTimeBare
-    cspice_timout,etEst,'YYYY MON DD HR:MN:SC.### TDB (SPICE TIMOUT)::TDB',999
-                ,TdbTimeSpice
-    catch,/cancel
-    UtcTime=UtcTimeBare + ' UTC (SPICE ET2UTC)'
-  endif else begin
-    catch,/cancel
-    UtcTime = 'LEAPSECOND-Kernel missing?'
-    TdbTimeSpice = UtcTime
-  endelse
-
-  xxx =
-  {            ET:  etEst
-  , MISS_DISTANCE:  cspice_vnorm(stOut[0:2])
-  ,         SPEED:  cspice_vnorm(stOut[3:5])
-  ,        TARGET:  targ
-  ,           OBS:  obs
-  ,   OBS_POS_J2k:  obsPosJ2k
-  ,   OBS_VEL_J2k:  obsVelJ2k
-  ,   SUN_POS_J2k:  sunPosJ2k
-  ,   SUN_AZEL_FB:  sunAzElRFb[0:1]
-  ,    SUN_STATUS:  sunStatus
-  ,       LT_CORR:  ltcorrOut
-  ,       TCA_ERR:  abs(delEst)
-  ,   CA_DIST_ERR:  abs(range-oldRange)
-  ,      JDTDBTCA:  jdTdbTca
-  ,    TDBTIMEIDL:  TdbTimeIdl
-  ,  CALTIMESPICE:  CalTimeSpice + ' TDB (SPICE ETCAL)'
-  ,  TDBTIMESPICE:  TdbTimeSpice
-  ,       UTCTIME:  UtcTime
-  }
-
-  tcaOut = create_struct( xxx
-  , 'tofCalc', findvinf( xxx
-                       , VinfTarg
-                       , VinfEtOffset
-                       , VinfEtStep
-                       , VinfEtNum
-                       , debug=debug
-                       )
-  )
-
-  if not keyword_set(nounloadkernels) then ftca_furnsh,kernels,/unload
-
-  return, tcaOut
-end
+"""
 ########################################################################
 ### FTCA_TEST:  FINDTCA support function
 #######################################################################n
@@ -328,9 +324,9 @@ end
 bsp='nh_ref_v_od059b_encounter_only.bsp'
 tls='naif0010.tls'
 
-testEr=0L
+testEr=0
 catch,testEr
-if testEr eq 0L then begin
+if testEr == 0 then begin
   caOut=ftca_test('999',kernels=[bsp],VinfTarg='999',/debug)
   catch,/cancel
 endif else begin
--- findvinf.py
+++ findvinf.py
@@ -2,6 +2,12 @@ import findtca
 import numpy as np
 import spiceypy as sp
 
+try: clight
+except:
+  clight = sp.clight()
+  spd = sp.spd()
+  dpr = sp.dpr()
+
 class FINDVINF:
   def __init__(self
               ,tcaInp
@@ -15,12 +21,12 @@ class FINDVINF:
     self.success, self.status, self.msg = False, 'Vinf CALC SKIPPED', ''
     if VinfTargetArg is None: return
 
-    etOffDefault = sp.spd() * 3.25
+    etOffDefault = spd * 3.25
 
     self.VinfTarget = str(VinfTargetArg).strip()
 
     try   : self.VinfEtOffs = float(VinfEtOffsArg)
-    except: self.VinfEtOffs = etoffDefault
+    except: self.VinfEtOffs = etOffDefault
 
     try   : self.VinfEtStep = float(VinfEtStepArg)
     except: self.VinfEtStep = 600e0
@@ -38,13 +44,11 @@ class FINDVINF:
                            )
 
       etTca = caOut.et
-      spd = sp.spd()
       n1 = self.VinfEtNum
       iw1 = np.arange(n1,dtype=np.int32)
-      n = n1*3L
-      iw23 = n+np.arange(n1*2L,dtype=np.int32)
-      t0s =  -rtn.VinfEtOffs + np.arange(n)*36d2
-      ts = etTca+t0s
+      n = n1*3
+      iw23 = n+np.arange(n1*2,dtype=np.int32)
+      ts = (np.arange(n)*36e2) + (etTca - rtn.VinfEtOffs)
       vsTarg = np.zeros(3*n,dtype=np.float64).reshape((-1,3,))
       vsObs = np.zeros(3*n,dtype=np.float64).reshape((-1,3,))
       try   : targNum = sp.bodn2c(caOut.Target)
@@ -58,123 +62,121 @@ class FINDVINF:
 
       baryName = sp.bodc2s(baryID)
 
-      for i in range(n):
-        vsTarg[i] = sp.spkezr(caOut.Target,ts[i],'j2000','none',baryName)
-        vsObs[i] = sp.spkezr(caOut.Obs,ts[i],'j2000','none',baryName)
+      vsTarg = np.vstack(
+         [sp.spkezr(caOut.Target,t,'j2000','none',baryName)[0][:2]
+          for t in ts
+         ])
+      vsObs = np.vstack(
+         [sp.spkezr(caOut.Obs,t,'j2000','none',baryName)[0][:2]
+          for t in ts
+         ])
+
       vs = vsTarg - vsObs
-      ABs = dindgen(2,3)
-ABsObs = dindgen(2,3)
-for i = 0L,2L do begin
-  vis = vs[i,iw1]
-  ABs[*,i] = linfit(t0s[iw1],vis[*],/double,yfit=yfits)
-  vis = vsObs[i,iw1]
-  ABsObs[*,i] = linfit(t0s[iw1],vis[*],/double,yfit=yfits)
-endfor
 
-vel = -transpose(ABs[1,*])             ;;; Target-relative Obs velocity
-velObs = transpose(ABsObs[1,*])        ;;; Bary-relative Obs velocity
-avgEt = mean(ts,/double)               ;;; Mean Time
-avgpos = total(vs,2,/double)/n         ;;; Mean S/C-relative target position
-avgPosObs = total(vsObs,2,/double)/n   ;;; Mean Bary-relative Obs position
+      a = np.vstack((np.ones(n),ts,))
 
-;;; find B-plane TCA 'BTCA' i.e. when B-plane passes through target
+      ### Linear fit time vs. position => Target-relative Obs velocity
+      ### Linear fit time vs. position => Bary-relative Obs vel => vInf
+      ### Mean Time
+      ### Mean Bary-relative Obs position
+      vel = - np.linalg.lstsq(a,vs,rcond=None)[0][1]
+      vInf = np.linalg.lstsq(a,vsObs,rcond=None)[0][1]
+      avgEt = ts.mean()
+      avgPosObs = vsObs.mean(axis=0)
 
-et = etTca
-dt = 0d0
-ltim2 = 0d0
-iter = 0L
-while ( iter lt 20L and abs(dt) gt 1d-3 ) or iter eq 0L do begin
-  iter = iter + 1
-  et = et + dt
-  cspice_spkezr,caOut.Target,et,'j2000','none',baryName,st,ltim ;;;Target @ BTCA
-  targWrtBary = st[0:2]
-  velTargWrtBary = st[3:5]
-  obsWrtBary = avgPosObs + velObs * (et + ltim2 - avgEt)
-  pos = targWrtBary - obsWrtBary
+      ### find B-plane TCA 'BTCA' i.e. when target is in B-plane
 
-  ;;; do one iteration on light time
-  ltim2 = cspice_vnorm(pos) / cspice_clight()
-  obsWrtBary = avgPosObs + velObs * (et + ltim2 - avgEt)
-  pos = targWrtBary - obsWrtBary
+      et = etTca
+      dt = 0e0
+      ltim2 = 0e0
+      iter = 0
+      while ( iter < 20 and abs(dt) > 1e-3 ) or iter == 0:
+        iter = iter + 1
+        et = et + dt
+        ###Target @ BTCA
+        ltim,st = sp.spkezr(caOut.Target,et,'j2000','none',baryName)
+        targWrtBary = st[0:2]
+        velTargWrtBary = st[3:5]
+        obsWrtBary = avgPosObs + vInf * (et + ltim2 - avgEt)
+        pos = targWrtBary - obsWrtBary
 
-  vel = velObs - velTargWrtBary
-  ;;; N.B. This is Vinfinity, not the observer@TCA velocity wrt the Target@TCA
-  dt = cspice_vdot(vel,pos) / cspice_vdot(vel,vel)  ;;; delta ET to BTCA
-endwhile
-if dodebug then help,iter,dt,ltim2,et-etTca
+        ### do one iteration on light time
+        ltim2 = sp.vnorm(pos) / clight
+        obsWrtBary = avgPosObs + vInf * (et + ltim2 - avgEt)
+        vTarg = targWrtBary - obsWrtBary
 
-velObsTca = vel
+        vel = vInf - velTargWrtBary
+        ### N.B. This is Vinfinity, not the observer@TCA velocity wrt the Target@TCA
+        dt = sp.vdot(vel,vTarg) / sp.vdot(vel,vel)  ### delta ET to BTCA
 
-;;; Xform from J2000 to B-plane frame, subscript bpl
-;;;
-;;; +Zbpl in J2000 frame  = > TOF   => Obs asymptotic velocity wrt Targ
-;;; +Ybpl in J2000 frame  = > BdotT => [Target position wrt Obs] cross +Zbpl
-;;; +Xbpl in J2000 frame  = > BdotR => Obs position vec wrt Targ @ TCA
+      vInfTca = vel
 
-cspice_vhat,  velObsTca, uvTof                 ;;; TOF    = > +Zbpl
-cspice_ucrss,  pos, uvTof, uvBdotT             ;;; BdotT  = > +Ybpl
-cspice_ucrss,  uvBdotT, uvTof, uvBdotR         ;;; BdotR  = > +Xbpl
+      ### Xform from J2000 to B-plane frame, subscript bpl
+      ###
+      ### +Zbpl in J2000 frame  = > TOF   => Obs asymptotic velocity wrt Targ
+      ### +Ybpl in J2000 frame  = > BdotT => [Target position wrt Obs] cross +Zbpl
+      ### +Xbpl in J2000 frame  = > BdotR => Obs position vec wrt Targ @ TCA
 
-;;; [+Xbpl, +Ybpl, +Zbpl]  = > Matrix to convert J2000 vectors to B-plane frame
+      uvTof = sp.vhat(vInfTca)                      ### TOF    = > +Zbpl
+      uvBdotT = sp.ucrss(vTarg, uvTof)              ### BdotT  = > +Ybpl
+      uvBdotR = sp.ucrss(uvBdotT, uvTof)            ### BdotR  = > +Xbpl
 
-mtx_j2b = double( [uvBdotR, uvBdotT, uvTof], 0, 3, 3)
+      ### [+Xbpl, +Ybpl, +Zbpl]  = > Matrix to convert J2000 vectors to B-plane frame
 
-cspice_recrad,uvTof,rTof,raTof,decTof
-cspice_recrad,uvBdotR,rBdotR,raBdotR,decBdotR
+      mtx_j2b = numpy.vstack((uvBdotR, uvBdotT, uvTof,))
 
-dpr = cspice_dpr()
+      rTof,raTof,decTof = sp.recrad(uvTof)
+      rBdotR,raBdotR,decBdotR - sp.recrad(uvBdotR)
 
-raTof = raTof * dpr
-decTof = decTof * dpr
 
-raBdotR = raBdotR * dpr
-decBdotR = decBdotR * dpr
+      raTof = raTof * dpr
+      decTof = decTof * dpr
 
-;;; Xform from barycentric target RTN frame to J2000
-;;; R  = > Radial from Barycenter to target
-;;; N  = > Normal to obit plane, R cross velocity
-;;; T  = > Along track, N cross R
+      raBdotR = raBdotR * dpr
+      decBdotR = decBdotR * dpr
 
-etRtn = tcaInp.et + etRtnOffset
-cspice_etcal,etRtn,rtnTDBCaldate
-cspice_spkezr,caOut.Target,etRtn,'j2000','none',baryName,stBary2TargTca,ltim
+      ### Xform from barycentric target RTN frame to J2000
+      ### R  = > Radial from Barycenter to target
+      ### N  = > Normal to orbit plane, R cross velocity
+      ### T  = > Along track, N cross R
 
-posBary2Targ = stBary2TargTca[0:2]
-velBary2Targ = stBary2TargTca[3:5]
+      etRtn = tcaInp.et + etRtnOffset
+      rtnTDBCaldate = sp.etcal(etRtn)
+      stBary2TargTca = sp.spkezr(caOut.Target,etRtn,'j2000','none',baryName)[0]
 
-cspice_vhat, posBary2Targ, uvRb2tt
-cspice_ucrss, uvRb2tt, velBary2Targ, uvNb2tt
-cspice_ucrss, uvNb2tt, uvRb2tt, uvTb2tt
+      posBary2Targ = stBary2TargTca[:3]
+      velBary2Targ = stBary2TargTca[3:]
 
-rtn = create_struct( rtn $
-, 'mtx_j2b', mtx_j2b $
-, 'raTof', raTof $
-, 'decTof', decTof $
-, 'raBdotR', raBdotR $
-, 'decBdotR', decBdotR $
-, 'bodyTcaBary2Targ', tcaInp.Target $
-, 'tdbBary2Targ', tcaInp.CALTIMESPICE $
-, 'posBary2Targ', posBary2Targ $
-, 'velBary2Targ', velBary2Targ $
-, 'mtx_RTNtarg2j', transpose( double([ uvRb2tt, uvTb2tt, uvNb2tt], 0, 3, 3)) $
-, 'tdbRTNCaldate', rtnTDBCaldate + ' TDB (SPICE ETCAL)' $
-, 'tdbRTN_sPastJ2k', etRtn $
-, 'Rtn_Offset_s', etRtnOffset $
-)
+      uvRb2tt = sp.vhat(posBary2Targ)
+      uvNb2tt = sp.ucrss(uvRb2tt, velBary2Targ)
+      uvTb2tt = sp.ucrss(vNb2tt, uvRb2tt)
 
-rtn.success = 1b
-rtn.status = 'Vinf CALC SUCCEEDED'
-message,'OK',continue = debug
+      self.mtx_j2b = mtx_j2b
+      self.raTof = raTof
+      self.decTof = decTof
+      self.raBdotR = raBdotR
+      self.decBdotR = decBdotR
+      self.bodyTcaBary2Targ = tcaInp.Target
+      self.tdbBary2Targ = tcaInp.CALTIMESPICE
+      self.posBary2Targ = posBary2Targ
+      self.velBary2Targ = velBary2Targ
+      self.mtx_RTNtarg2j = numpy.vstack((uvRb2tt, uvTb2tt, uvNb2tt,)).T
+      self.tdbRTNCaldate = rtnTDBCaldate + ' TDB (SPICE ETCAL)'
+      self.tdbRTN_sPastJ2k = etRtn
+      self.Rtn_Offset_s = etRtnOffset
+
+      self.success = True
+      self.status = 'Vinf CALC SUCCEEDED'
+      self.msg = 'OK'
 
     except:
-       ftca_furnsh(kernels, /unload)
+       ftca_furnsh(kernels, unload=True)
        self.msg = traceback.format_exc()
 
-end
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; FVINF_TEST:  FINDTCA support function
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;n
+"""
+########################################################################
+### FVINF_TEST:  FINDTCA support function
+#######################################################################n
 function fvinf_test, targ, obs, kernels = kernels $
              , debug = debug
 
@@ -188,17 +190,17 @@ function fvinf_test, targ, obs, kernels = kernels $
 
 end
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+########################################################################
 
 ks = ['nh_ref_v_od059b_encounter_only.bsp']
 
 !quiet = 1
-testEr = 0L
+testEr = 0
 catch,testEr
 
-if testEr eq 0L then begin
+if testEr eq 0 then begin
   pcaOut = fvinf_test('999',kernels=ks)
-  pvinfOut = findvinf(pcaOut,'999',kernels=ks,/debug,etRtnOffset=-6d2)
+  pvinfOut = findvinf(pcaOut,'999',kernels=ks,/debug,etRtnOffset=-6e2)
   cvinfOut = findvinf(pcaOut,'901',kernels=ks,/debug)
   nvinfOut = findvinf(pcaOut,'902',kernels=ks,/debug)
   hvinfOut = findvinf(pcaOut,'903',kernels=ks,/debug)
@@ -211,3 +213,4 @@ endelse
 help,/st,pvinfOut
 
 end
+"""
